a1b0d892843f8cf250a502614213ae77aae09946
DarthDippy <lucaspfaff79@gmail.com>
2015-03-06 07:58:54 -0500

    code cleanup. testing against new grunt build

--- regulations/management/commands/cache_webpages.py
+++ regulations/management/commands/cache_webpages.py
@@ -8,43 +8,39 @@ import time
 
 
 class Command(BaseCommand):
-    args = "<url where eregulations is located> <optional- comma seperated list of regulation locations to be parsed >  "
+    args = "<url where eregulations is located> <optional- comma seperated list of regulation locations to be parsed >"
     help = 'call every page in eregulations allowing the pages to be cached'
 
-    def access_url(obj,*args):
+    def access_url(self, *args):
 
         try:
-            without_anchor = args[0].split("#")[0]
-            req = requests.get(without_anchor)
+            req = requests.get(args[0])
 
             status_txt = "Cached"
             if req.status_code != 200:
-                status_txt =  "Failed"
+                status_txt = "Failed"
 
-            msg =  "{0} (status {1}): {2}".format(status_txt, str(req.status_code)  , req.url)
-            obj.stdout.write(msg)
+            msg = "{0} (status {1}): {2}".format(status_txt, str(req.status_code), req.url)
+            self.stdout.write(msg)
         except Exception, errtxt:
-            obj.stderr.write("    Failed: " + args[0])
-            obj.stderr.write( "    " + str(errtxt))
-
+            self.stderr.write("    Failed: " + args[0])
+            self.stderr.write("    " + str(errtxt))
 
     def get_main_reg_list(self):
         try:
             regulations_links = []
-            html = requests.get( self.full_url ).text
+            html = requests.get(self.full_url).text
             soup = BeautifulSoup(html)
-            reg_list = soup.find( "ul", { "class" : "reg-list" } )
+            reg_list = soup.find("ul", {"class": "reg-list"})
 
             for link in reg_list.findAll("li"):
                 regulations_links.append(link.find("a")["href"])
 
-
             return regulations_links
 
         except Exception, errtxt:
-            self.stderr.write( "main page failed to load")
-            self.stderr.write( "    " + str(errtxt))
-
+            self.stderr.write("main page failed to load")
+            self.stderr.write("    " + str(errtxt))
 
     #figure out the partial path depending on root location of eregs
     def get_partial_url(self,  href):
@@ -52,14 +48,15 @@ class Command(BaseCommand):
         href = href.split("/")
 
         if self.full_url != self.base_url:
-            href[1] = href[1] + "/partial"
+            href[1] += "/partial"
         else:
-            href[0] = href[0] + "/partial"
+            href[0] += "/partial"
 
         href = '/'.join(href)
 
-        return self.base_url  + href
+        href = href.split("#")[0]
 
+        return self.base_url + href
 
     def handle(self, *args, **options):
 
@@ -67,8 +64,8 @@ class Command(BaseCommand):
         self.base_url = url.scheme + "://" + url.netloc
         self.full_url = sys.argv[2]
 
-        self.stdout.write("Base Url:"+ self.base_url)
-        self.stdout.write("eRegs location:"+ self.full_url)
+        self.stdout.write("Base Url:" + self.base_url)
+        self.stdout.write("eRegs location:" + self.full_url)
 
         self.regulations = None
 
@@ -90,14 +87,14 @@ class Command(BaseCommand):
 
                 for a in reg_soup:
                     if a.has_key('data-section-id'):
-
-                        partial = self.get_partial_url( a["href"])
+                        partial = self.get_partial_url(a["href"])
 
                         thread_count = len(threading.enumerate())
 
                         #process 5 web pages at time. Some servers hate a lot of requests at once.
+                        #slow it down so the threads can catch up
                         if thread_count <= 5:
-                            threading.Thread(target=self.access_url, args=(partial, ) ).start()
+                            threading.Thread(target=self.access_url, args=(partial, )).start()
                         else:
                             self.stdout.write("Currently Processing " + str(thread_count) + " Urls")
                             self.stdout.write("Waiting...")
@@ -109,9 +106,3 @@ class Command(BaseCommand):
 
         except Exception, errtxt:
             self.stderr.write("    " + str(errtxt))
-
-
-
-
-
-
